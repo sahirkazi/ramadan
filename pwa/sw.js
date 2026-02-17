e74e646ea2ab663b0604b98163beaa30dd6869ab
sahirkazi <sahirkazi95@gmail.com>
2026-02-17 23:28:00 +0530

    PWA app setup

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = "ramadan-v4";
 const ASSETS = [
-  "./",
-  "dua",
-  "surah",
-  "tasbih",
-  "about",
-  "style.css",
-  "data.js",
-  "manifest.json",
-  "favicon.png",
-  "pwa-192x192.png",
-  "pwa-512x512.png"
+  "../",
+  "../dua",
+  "../surah",
+  "../tasbih",
+  "../about",
+  "../style.css",
+  "../data.js",
+  "./manifest.json",
+  "./favicon.png",
+  "./pwa-192x192.png",
+  "./pwa-512x512.png"
 ];
 
 self.addEventListener("install", (e) => {
